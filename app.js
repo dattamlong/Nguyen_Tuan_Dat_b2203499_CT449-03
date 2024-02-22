b25d155ec33aeb6a7cd76f7b45746f6e26732e08
dat nguyen <ndat9124@mail.com>
2024-02-22 09:09:56 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const contactsRouter = require("./app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
@@ -12,6 +12,6 @@ app.get("/", (req, res) =>
   res.json({ message: "Welcome to contact book application" })
 );
 
-// app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
